a5f7545d0fff0926c0d03662715499dde4288bf4
Bhaskar Sengupta <sajal1943@gmail.com>
2021-04-02 15:16:22 +0530

    home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,17 +3,27 @@ import Carousel from "react-material-ui-carousel";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import './home.css'
+import Slider from "react-slick";
 import Card from './Card'
+import Owl from './Owl_Car'
 const Home = () => {
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1
+      };
+
     return (
         <div className="body">
         <div className="home">
         <div className="home_container">
             <Carousel autoPlay animation="slide">
           <div className="carousel_design">
-            <img
+            <img 
               className="home_carousal"
-              src="https://via.placeholder.com/1250x500/0000FF/808080?Text=Digital.com"
+              src="https://images.unsplash.com/photo-1574943320219-553eb213f72d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFybWVyfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
               alt="Carousal here"
             />
           </div>
@@ -59,9 +69,11 @@ const Home = () => {
         </div>
        </div>
 
-       
-         </div>
+        <br></br>
         
+    
+    </div>   
+  
         </div>
        
         </div>
