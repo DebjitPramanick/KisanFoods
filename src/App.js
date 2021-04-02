ec28b6d40fc47cf530c1c96cd36f98e3ddce6cc6
Bhaskar Sengupta <sajal1943@gmail.com>
2021-04-02 13:49:33 +0530

    first

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css"
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Login from './Pages/Auth/Login'
 import Register from './Pages/Auth/Register'
-
+import Home from './Pages/Home/Home'
 const App = () => {
     return (
         <div>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,69 @@
 import React from 'react'
-
+import Carousel from "react-material-ui-carousel";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import './home.css'
+import Card from './Card'
 const Home = () => {
     return (
-        <div>
-            hi
+        <div className="body">
+        <div className="home">
+        <div className="home_container">
+            <Carousel autoPlay animation="slide">
+          <div className="carousel_design">
+            <img
+              className="home_carousal"
+              src="https://via.placeholder.com/1250x500/0000FF/808080?Text=Digital.com"
+              alt="Carousal here"
+            />
+          </div>
+
+          <img
+            className="home_carousal"
+            src="https://via.placeholder.com/1250x500/FFFF00/000000"
+            alt="Carousal here"
+          />
+
+          <div className="carousel_design">
+            <img
+              className="home_carousal"
+              src="https://via.placeholder.com/1250x500"
+              alt="Carousal here"
+            />
+          </div>
+        </Carousel>
+       
+       <br></br><br></br>
+
+       <div className="row ">
+       <div className="col-sm-4">
+        <Card />
+        </div>
+        <div className="col-sm-4">
+        <Card />
+        </div>
+        <div className="col-sm-4">
+        <Card />
+        </div>
+       </div>
+        <br></br>
+       <div className="row">
+       <div className="col-sm-4">
+        <Card />
+        </div>
+        <div className="col-sm-4">
+        <Card />
+        </div>
+        <div className="col-sm-4">
+        <Card />
+        </div>
+       </div>
+
+       
+         </div>
+        
+        </div>
+       
         </div>
     )
 }
